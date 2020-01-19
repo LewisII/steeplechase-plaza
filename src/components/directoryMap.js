be6770b58388709a5d594a34a2dd81971ca95bbd
Lewis Amos <Lewamos2@gmail.com>
2020-01-19 14:08:06 -0600

    updated directory map to extend react component

--- src/components/directoryMap.js
+++ src/components/directoryMap.js
@@ -1,28 +1,32 @@
 import React from "react"
 
-const DirectoryMap = () => {
 
-    const businessesArray = [
-        { name: "Church Street Bar", location: "435 S Church St, St Peters, MO 63376"},
-        { name: "Merry Maids", location: "433 S Church St, St Peters, MO 63376" },
-        { name: "Zak's Barber Shop", location: "431 S Church St, St Peters, MO 63376" },
-        { name: "Restraunt(need Name)", location: "429 S Church St, St Peters, MO 63376" },
-        { name: "Moving Company(need Name)", location: "427 S Church St, St Peters, MO 63376" },
-        { name: "Eye Candy Rentals", location: "425 S Church St, St Peters, MO 63376" },
-        { name: "Convenant Garage Door", location: "423 S Church St, St Peters, MO 63376" },
-        { name: "Cosmic Liquor", location: "421 S Church St, St Peters, MO 63376" },
-        { name: "The Ink Spot", location: "419 S Church St, St Peters, MO 63376" }];
+const businessesArray = [
+    { name: "Church Street Bar", location: "435 S Church St, St Peters, MO 63376" },
+    { name: "Merry Maids", location: "433 S Church St, St Peters, MO 63376" },
+    { name: "Zak's Barber Shop", location: "431 S Church St, St Peters, MO 63376" },
+    { name: "Restraunt(need Name)", location: "429 S Church St, St Peters, MO 63376" },
+    { name: "Moving Company(need Name)", location: "427 S Church St, St Peters, MO 63376" },
+    { name: "Eye Candy Rentals", location: "425 S Church St, St Peters, MO 63376" },
+    { name: "Convenant Garage Door", location: "423 S Church St, St Peters, MO 63376" },
+    { name: "Cosmic Liquor", location: "421 S Church St, St Peters, MO 63376" },
+    { name: "The Ink Spot", location: "419 S Church St, St Peters, MO 63376" }];
 
-    const namesList = businessesArray.map(name => {
-        return (
-            <li>{name.name}</li>
-        )
-    })
+const namesList = businessesArray.map(name => {
+    return (
+        <li>{name.name}</li>
+    )
+})
 
+class DirectoryMap extends React.Component {
+    state = {}
+    
+    render() {
     return (
     <ul>
         {namesList}
     </ul>
     );
+    }
 }
  export default DirectoryMap
