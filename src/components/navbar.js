7c830d34f081cdad6d1a805e9ade999d57a8aa33
Lewis Amos <Lewamos2@gmail.com>
2020-01-20 00:15:34 -0600

    cleaned up extra files and began converting data to arrays from hard coding

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,14 +2,26 @@ import React from "react"
 import { Link } from "gatsby"
 
 const NavBar = () => {
+  /*const webLinks = [
+    { address: "/", title: "HomePage" },
+    { address: "/businesses/", title: "Businesses" },
+    { address: "/location/", title: "Where are we" },
+  ]
+  */
     return (
-        <>
-            <nav style = { {listStyleType: "none"} } >
-                <li><Link to="/">Homepage</Link></li>
-                <li><Link to="/businesses/">Businesses</Link></li>
-                <li><Link to="/location/">Where are we</Link></li>
-            </nav>
-        </>
+      <>
+        <ul id="navBar" style={{ listStyleType: "none" }}>
+          <li style={{display: "inline" }}>
+            <Link to="/">Homepage</Link>
+          </li>
+          <li style={{ display: "inline" }}>
+            <Link to="/businesses/">Businesses</Link>
+          </li>
+          <li style={{ display: "inline" }}>
+            <Link to="/location/">Where are we</Link>
+          </li>
+        </ul>
+      </>
     )
 
 }
--- src/pages/businesses.js
+++ src/pages/businesses.js
@@ -3,18 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import DirectoryMap from "../components/directoryMap"
-const businessesArray = [
+/**const businessesArray = [
     {name: "Church Street Bar"},
     {name: "The Ink Spot"},
     {name: "Zak's Barber Shop"},
     {name: "Cosmic Liquor"},
     {name: "Athletic Advantage"}];
-    
+    */
 const BusinessesPage = () => (
     <Layout>
         <SEO title="Businesses" />
         <h1>Business Directory</h1>
-        <DirectoryMap businessesArray/>
+        <DirectoryMap/>
     </Layout>
 )
 
