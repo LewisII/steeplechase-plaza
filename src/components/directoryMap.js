867c67507c42cfe8fdc42231dfe901d6035aed77
Lewis Amos <Lewamos2@gmail.com>
2020-01-22 18:33:40 -0600

    made visual changes to data

--- src/components/directoryMap.js
+++ src/components/directoryMap.js
@@ -1,21 +1,26 @@
 import React from "react"
+import "./directoryMap.css"
 
 const DirectoryMap = data => {
   return (
-    <div className="bussinessCard" width="250px">
+    <div id="directoryMap">
       {data.store.map(store => {
         return (
-          <p>
-            {store.name} <br /> {store.location}
-            <br />
-            Phone number: {store.telephone}
-            <br />
-            {store.info}
-            <br />
-          </p>
+          <div className="storeCard" width="50px">
+            <p>
+              <h2> {store.name} </h2>
+              {store.location}
+              <br />
+              Phone number: {store.telephone}
+              <br />
+              {store.info}
+              <br />
+            </p>
+          </div>
         )
       })}
     </div>
   )
 }
+
 export default DirectoryMap
