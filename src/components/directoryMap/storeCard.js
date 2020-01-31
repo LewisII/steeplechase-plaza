97cb481016cd69a94812e18ae8db4c2975c6e887
Lewis Amos <Lewamos2@gmail.com>
2020-01-30 20:36:36 -0600

    cleaned up file removed extra file

--- src/components/directoryMap/storeCard.js
+++ src/components/directoryMap/storeCard.js
@@ -1,23 +1,31 @@
 import React from 'react'
+import './storeCard.css'
+
+
 class StoreCard extends React.Component {
   render() {
     return (
-      <div className="storeCard" width="50px">
+      <div className="storeTile">
         <p>
-          <h2>
-            {" "}
-            <a href={this.props.store.website}>{this.props.store.name}</a>
-          </h2>
-          <a href={this.props.store.googleMapsLink}>{this.props.store.location}</a>
+          <h2>{this.props.store.name}</h2>
+          <hr />
+          <a href={this.props.store.googleMapsLink}>
+            {this.props.store.location}
+          </a>
           <br />
           Phone number:{" "}
-          <a href={"tel:" + this.props.store.telephone}>{this.props.store.telephone}</a>
+          <a href={"tel:" + this.props.store.telephone}>
+            {this.props.store.telephone}
+          </a>
+          <br />
+          <a href={this.props.store.website}>{this.props.store.website}</a>
           <br />
           {this.props.store.info}
           <br />
           <img
             src={this.props.store.image_storeFront}
             alt={"Store Front of " + this.props.store.name}
+            className="cardImage"
           />
         </p>
       </div>
--- src/components/image/optimizedImage.js
+++
@@ -1,9 +0,0 @@
-export const optimizedImage = graphql`
-  fragment squareImage on File {
-    childImageSharp {
-      fluid(maxWidth: 200, maxHeight: 200) {
-        ...GatsbyImageSharpFluid
-      }
-    }
-  }
-`
